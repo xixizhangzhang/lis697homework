a369a65bc9238370821d9ea975479ebf65b2e29b
Xi Zhang <zhangx0303@gmail.com>
2017-02-10 09:50:46 -0500

    Finally finish the first challenge
    I spend to much time on first challenge

--- codehw2.php
+++ codehw2.php
@@ -10,7 +10,9 @@
 </head>
 <body>
 <?php
-$ISBN = 9992158107;
+echo "<p class=\"header\">Challenge #1: ISBN Validation";
+//define ISBN as a string
+$ISBN = "996215820X";
 // $check10 = (int)($ISBN / 1000000000)*10;
 // $check9 = (int)(($ISBN - $check10)/1000000000)*9;
 // $check8 = (int)(($ISBN - $check10 - $check9)/100000000)*8;
@@ -21,22 +23,48 @@ $ISBN = 9992158107;
 // $check3 = (int)(($ISBN - $check10 - $check9 - $check8 - $check7 - $check6 - $check5 - $check4)/1000)*3;
 // $check2 = (int)(($ISBN - $check10 - $check9 - $check8 - $check7 - $check6 - $check5 - $check4 - $check3)/100)*2;
 // $check1 = (int)(($ISBN - $check10 - $check9 - $check8 - $check7 - $check6 - $check5 - $check4 - $check3 - $check2)/10)*1;
-$digit = 9;
-$minus = 0;
-$check1 = 0;
-$check2 = 0;
-$total = 0;
-if ($digit == 9) {
-  $check1 = (int)(($ISBN) / pow(10, $digit)) * ($digit+1);
+// $digit = 9;
+// $minus = 0;
+// $check1 = 0;
+// $check2 = 0;
+// $total = 0;
+// if ($digit == 9) {
+//   $check1 = (int)(($ISBN) / pow(10, $digit)) * ($digit+1);
+// }
+//
+// for ($digit == 8; $digit >= 0; --$digit) {
+//   $minus = $minus + (int)($ISBN / pow(10, $digit+2)) * pow (10, $digit+2);
+//   $check2 = $check2 + (int)(($ISBN - $minus) / pow(10, $digit)) * ($digit+1);
+// }
+// $total = $check1 + $check2;
+//
+// echo "you have $total";
+
+// $string_isbn = "$ISBN";
+$total8 = 0;
+//a for loop to check digit number in each place except the last digit, and then add them together
+for ($count=0; $count<9; ++$count) {
+    $each_check = substr($ISBN, $count, 1) * (10-$count);
+    $total8 = $total8 + $each_check;
+}
+//a if condition to check the last digit, if last digit is equal to X, then use 10
+if (substr($ISBN, 9, 1) == "X") {
+    $check9 = 10 * 1;
+} else {
+  $check9 = substr($ISBN, 9, 1) * 1;
+}
+//add all together
+$total = $total8 + $check9;
+//a if condition to check if the ISBN is validity
+if ($total % 11 == 0) {
+  echo "<p>Checking isbn $ISBN for validity......";
+  echo "<p>This is a valid ISBN!";
+} else {
+  echo "<p>Checking isbn $ISBN for validity......";
+  echo "<p>This is NOT a valid ISBN!";
 }
 
-for ($digit == 8; $digit >= 0; --$digit) {
-  $minus = $minus + (int)($ISBN / pow(10, $digit+2)) * pow (10, $digit+2);
-  $check2 = $check2 + (int)(($ISBN - $minus) / pow(10, $digit)) * ($digit+1);
-}
-$total = $check1 + $check2;
 
-echo "you have $total";
 
 ?>
 </body>
